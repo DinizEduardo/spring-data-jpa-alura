036a811c8ecb614f5a2fe7d8cc6f6005d8166833
DinizEduardo <eduardo.diniz987@gmail.com>
2021-02-12 11:28:24 -0300

    Criação das tabelas de unidade e relacionamentos

--- src/main/java/br/com/alura/spring/data/orm/Funcionario.java
+++ src/main/java/br/com/alura/spring/data/orm/Funcionario.java
@@ -1,7 +1,11 @@
 package br.com.alura.spring.data.orm;
 
+import org.hibernate.annotations.Fetch;
+import org.hibernate.annotations.FetchMode;
+
 import javax.persistence.*;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Table(name = "funcionarios")
@@ -18,6 +22,20 @@ public class Funcionario {
     @ManyToOne
     @JoinColumn(name = "cargo_id", nullable = false)
     private Cargo cargo;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "funcionarios_unidades",
+            joinColumns = { @JoinColumn(name = "unidade_id") },
+            inverseJoinColumns = {@JoinColumn(name = "funcionario_id")})
+    private List<Unidade> unidades;
+
+    public List<Unidade> getUnidades() {
+        return unidades;
+    }
+
+    public void setUnidades(List<Unidade> unidades) {
+        this.unidades = unidades;
+    }
 
     @Override
     public String toString() {
---
+++ src/main/java/br/com/alura/spring/data/orm/Unidade.java
@@ -0,0 +1,66 @@
+package br.com.alura.spring.data.orm;
+
+import org.hibernate.annotations.Fetch;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "unidades")
+public class Unidade {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String descricao;
+
+    private String endereco;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "funcionarios_unidades",
+            joinColumns = { @JoinColumn(name = "funcionario_id") },
+            inverseJoinColumns = {@JoinColumn(name = "unidade_id")})
+    private List<Funcionario> funcionarios;
+
+    public List<Funcionario> getFuncionarios() {
+        return funcionarios;
+    }
+
+    public void setFuncionarios(List<Funcionario> funcionarios) {
+        this.funcionarios = funcionarios;
+    }
+
+    @Override
+    public String toString() {
+        return "Unidade{" +
+                "id=" + id +
+                ", descricao='" + descricao + '\'' +
+                ", endereco='" + endereco + '\'' +
+                '}';
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+}
